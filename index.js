aca1cf5efd98dad8fffd93b7f49a8154b2bf55a4
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-20 13:10:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function viewCart() {
       array.push(`${cart[i]['itemName']} at $${cart[i]['itemPrice']}`)
     }
     array.push(`and ${cart[cart.length-1]['itemName']} at $${cart[cart.length-1]['itemPrice']}.`)
-    return array
+    return array.join(', ')
   }
 }
 
