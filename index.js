c4d4b0ac7f664a35f8b04748660fd45000b9a13a
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-20 13:53:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function total() {
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
     if (cart[i]['itemName'] === item) {
-      delete cart[i]
+      cart.splice(i, 1)
       return cart
     }
   }
